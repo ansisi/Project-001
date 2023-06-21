0ca727d11888acee0ff428728d79056b7585e425
ansisi <sihyeona73@gmail.com>
2023-06-22 06:14:04 +0900

    동물 복구

--- Assets/02.Scripts/RandomBox2.cs
+++ Assets/02.Scripts/RandomBox2.cs
@@ -41,14 +41,14 @@ public class RandomBox2 : MonoBehaviour
                 if (Cspawn == 1)
                 {
                     GameObject temp10 = Instantiate(Animal[14]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(14);
                     Debug.Log("고양이");
                 }
                 else if (Cspawn == 2)
                 {
                     GameObject temp10 = Instantiate(Animal[13]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(13);
                     Debug.Log("개미");
 
@@ -61,55 +61,58 @@ public class RandomBox2 : MonoBehaviour
                 else if (Cspawn == 4)
                 {
                     GameObject temp10 = Instantiate(Animal[15]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(15);
                     Debug.Log("닭");
                 }
                 else if (Cspawn == 5)
                 {
                     GameObject temp10 = Instantiate(Animal[22]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(22);
                     Debug.Log("연지벌레");
                 }
                 else if (Cspawn == 6)
                 {
                     GameObject temp10 = Instantiate(Animal[17]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(17);
                     Debug.Log("소");
                 }
                 else if (Cspawn == 7)
                 {
                     GameObject temp2 = Instantiate(Animal[1]);
-                    temp2.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp2.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(1);
                     Debug.Log("개");
                 }
                 else if (Cspawn == 8)
                 {
                     GameObject temp3 = Instantiate(Animal[2]);
-                    temp3.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp3.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(2);
                     Debug.Log("여우");
                 }
                 else if (Cspawn == 9)
                 {
                     GameObject temp4 = Instantiate(Animal[3]);
-                    temp4.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp4.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(3);
                     Debug.Log("기니피그");
                 }
                 else if (Cspawn == 10)
                 {
                     GameObject temp5 = Instantiate(Animal[7]);
-                    temp5.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp5.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(7);
                     Debug.Log("돼지");
                 }
                 else if (Cspawn == 11)
                 {
-                    Debug.Log("Sheep");
+                    GameObject temp5 = Instantiate(Animal[7]);
+                    temp5.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+                    GameManager.myAnimal.Add(7);
+                    Debug.Log("양");
                 }
                 else if (Cspawn == 12)
                 {
@@ -123,35 +126,35 @@ public class RandomBox2 : MonoBehaviour
                 if (Rspawn == 1)
                 {
                     GameObject temp10 = Instantiate(Animal[23]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(23);
                     Debug.Log("기린");
                 }
                 else if (Rspawn == 2)
                 {
                     GameObject temp10 = Instantiate(Animal[20]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(20);
                     Debug.Log("판다");
                 }
                 else if (Rspawn == 3)
                 {
                     GameObject temp7 = Instantiate(Animal[5]);
-                    temp7.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp7.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(5);
                     Debug.Log("원숭이");
                 }
                 else if (Rspawn == 4)
                 {
                     GameObject temp11 = Instantiate(Animal[10]);
-                    temp11.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp11.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(10);
                     Debug.Log("얼룩말");
                 }
                 else if (Rspawn == 5)
                 {
                     GameObject temp10 = Instantiate(Animal[18]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(18);
                     Debug.Log("악어");
                 }
@@ -166,13 +169,13 @@ public class RandomBox2 : MonoBehaviour
                 else if (Rspawn == 8)
                 {
                     GameObject temp6 = Instantiate(Animal[4]);
-                    temp6.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp6.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(4);
                 }
                 else if (Rspawn == 9)
                 {
                     GameObject temp10 = Instantiate(Animal[12]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(12);
 
                     Debug.Log("독수리");
@@ -180,7 +183,7 @@ public class RandomBox2 : MonoBehaviour
                 else if (Rspawn == 10)
                 {
                     GameObject temp10 = Instantiate(Animal[21]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(21);
 
                     Debug.Log("고래");
@@ -188,7 +191,7 @@ public class RandomBox2 : MonoBehaviour
                 else if (Rspawn == 11)
                 {
                     GameObject temp10 = Instantiate(Animal[9]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(9);
 
                     Debug.Log("호랑이");
@@ -204,13 +207,13 @@ public class RandomBox2 : MonoBehaviour
                 else if (Espawn == 2)
                 {
                     GameObject temp8 = Instantiate(Animal[6]);
-                    temp8.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp8.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(6);
                 }
                 else if (Espawn == 3)
                 {
                     GameObject temp10 = Instantiate(Animal[16]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(16);
 
                     Debug.Log("멍게");
@@ -230,7 +233,7 @@ public class RandomBox2 : MonoBehaviour
                 else if (Espawn == 7)
                 {
                     GameObject temp9 = Instantiate(Animal[8]);
-                    temp9.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp9.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(8);
                 }
 
@@ -241,7 +244,7 @@ public class RandomBox2 : MonoBehaviour
                 if (Lspawn == 1)
                 {
                     GameObject temp10 = Instantiate(Animal[11]);
-                    temp10.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp10.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(11);
 
                     Debug.Log("청룡");
@@ -254,14 +257,16 @@ public class RandomBox2 : MonoBehaviour
                 else if (Lspawn == 3)
                 {
                     GameObject temp3 = Instantiate(Animal[24]);
-                    temp3.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp3.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(24);
+
+                    Debug.Log("에일리언");
                 }
                 else if (Lspawn == 4)
                 {
 
                     GameObject temp = Instantiate(Animal[0]);
-                    temp.transform.position = new Vector3(0.0f, 2.0f, 0.0f);
+                    temp.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
                     GameManager.myAnimal.Add(0);
                 }
         
